77981c8c4023b892be55b3b2ea093b8d715e88b4
Glitch (poppyorchard-climate-change1) <none>
2019-06-13 20:22:40 +0000

    🍄🐏 Checkpoint ./house.css:5008387/57 ./index.html:5008350/79:5008388/611 ./house.html:5008387/4 ./Garage.js:5008387/565 ./Garage.css:5008387/39 ./kitchen.js:5008387/1420 ./Garage.html:5008387/263

--- Garage.js
+++ Garage.js
@@ -1,14 +1,14 @@
 var gasCarButton = document.getElementById("gasCar");
 gasCarButton.addEventListener("click", function()
   {
-  document.getElementById("text").innerHTML = "Gas-powered car stuff"
+  document.getElementById("text").innerHTML = "Gas cars cover wider distances and are cheaper than electric cars. But, they have many environmental cons. Gas leaks can lead to potential health hazards and the cars rely on fossil fuels, contributing to pollution and climate change."
   }
 );
 //gas car
 var electricCarButton = document.getElementById("electricCar");
 electricCarButton.addEventListener("click", function()
   {
-  document.getElementById("text").innerHTML = "Electric car stuff"
+  document.getElementById("text").innerHTML = "On average, it costs about half as much to drive an electric vehicle compared to a gas powered one. Electricity is less expensive than gasoline and EVs are more efficient than gasoline vehicles. However, electric cars have limited ranges, longer fuel/charge times, and they cost more."
   }
 );
 //electric car
--- kitchen.js
+++ kitchen.js
@@ -1,28 +1,28 @@
 var ovenButton = document.getElementById("oven");
 ovenButton.addEventListener("click", function()
   {
-  document.getElementById("text").innerHTML = "Oven stuff"
+  document.getElementById("text").innerHTML = "Ovens can use a lot of energy depending on the temperature set, and most cooking is done at 300 to 425 Fahrenheit degrees. Ovens use 1000 to 5000 watts, with an average modern oven using around 2400 watts on medium to high heat. To save energy when using the oven, try to cook in batches, cook more food, and freeze/refrigerate the leftovers!"
   }
 );
 //oven
 var stoveButton = document.getElementById("stove");
 stoveButton.addEventListener("click", function()
   {
-  document.getElementById("text").innerHTML = "Stove stuff"
+  document.getElementById("text").innerHTML = "The energy use of a stove top varies- smaller units will use 1000 watts while a larger heating element will go up to 3000 watts. An average modern stove top element will use about 1500 watts per an hour of heating on medium to high."
   }
 );
 //stove
 var dishwasherButton = document.getElementById("dishwasher");
 dishwasherButton.addEventListener("click", function()
   {
-  document.getElementById("text").innerHTML = "Dishwasher stuff"
+  document.getElementById("text").innerHTML = "Modern dishwashers are energy efficient and do not use a lot of water. Dishwashers use between 1200 and 2400 watts of power, with an average dishwasher using 1800 watts. Dishwashers will typically use less water than hand washing if you are washing a full load of dishes, so hand washing isn’t necessarily “better.” Make sure to always load your dishwasher fully!"
   }
 );
 //dishwasher
 var fridgeButton = document.getElementById("fridge");
 fridgeButton.addEventListener("click", function()
   {
-  document.getElementById("text").innerHTML = "Fridge stuff"
+  document.getElementById("text").innerHTML = "Modern fridges are very energy efficient compared to older models from a few decades ago. A fridge will use anywhere from 100 to 400 watts depending on size, and a large fridge will use about 180 watts annually. If you are using an old fridge it’s recommended you upgrade to a more efficient modern model. An easy way to reduce your energy for fridges is to make sure you don’t leave the fridge door open when it’s not in use."
   }
 );
 //fridge
